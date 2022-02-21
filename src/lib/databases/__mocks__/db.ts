e05c296bf370d69bc3a0e32673ec14b4975a0b62
zrecovery <zrecovery@outlook.com>
2022-02-21 22:33:32 +0800

    [测试]添加测试

---
+++ src/lib/databases/__mocks__/db.ts
@@ -0,0 +1,13 @@
+import { ArticleStore, IdxArticleStore } from '../db'
+import fakeIndexedDb from 'fake-indexeddb'
+import IDBKeyRange from 'fake-indexeddb/lib/FDBKeyRange'
+import { vitest } from 'vitest'
+import Article from '../article'
+
+vitest.unmock("../db")
+
+export { IdxArticleStore }
+
+export const mockIdxArticleStore = new IdxArticleStore({ indexedDB: fakeIndexedDb, IDBKeyRange: IDBKeyRange })
+
+export const mockArticleStore = new ArticleStore(mockIdxArticleStore)
---
+++ src/test/db.test.ts
@@ -0,0 +1,41 @@
+import Article from "../lib/databases/article";
+
+import { beforeEach, test, expect, vitest, assert } from "vitest"
+import { mockArticleStore, mockIdxArticleStore } from "../lib/databases/__mocks__/db";
+
+let mockSavedArticles  = async () => {
+    let mockArticles = [
+        new Article("mock title1", "mock book1", "mock  author1", "mock content1\nmock line2-1", 1.0),
+        new Article("mock title2", "mock book1", "mock  author1", "mock content2\nmock line2-1", 2.0),
+        new Article("mock title3", "mock book2", "mock  author2", "mock content3\nmock line2-3", 2.0)
+    ]
+    return await Promise.all(mockArticles.map(async article => {
+         article.id = await mockIdxArticleStore.articles.add(article)
+         return article
+    }))
+}
+
+test("能否保存文章", async () => {
+    let article = new Article("test title", "test book", "test author", "test content\ntest line2", 1.0)
+    let id =  await mockArticleStore.save(article)
+    expect(id).toBeDefined()
+    expect(id).toBeGreaterThanOrEqual(0)
+})
+
+test("能否读取文章",async () => {
+    let exceptedArticles = await mockSavedArticles()
+    exceptedArticles.map(async(testArticle) => expect(await mockArticleStore.get(testArticle.id)).toEqual(testArticle))
+})
+
+
+// TODO
+test("能否删除文章",async () => {
+    let moeckedArticles = await mockSavedArticles()
+
+})
+
+// TODO
+test("能否批量读文章",async () => {
+    let moeckedArticles = await mockSavedArticles()
+
+})
